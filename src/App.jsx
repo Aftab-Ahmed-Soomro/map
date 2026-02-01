c846b6ec7a0463511e7e1efbe9cd54030d02f825
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2026-02-01 14:46:00 +0500

    Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import { Icon } from 'leaflet'
+import { MapPin } from 'lucide-react'
 
 const App = () => {
+
+  // markers
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "Hello, I am pop up 1"
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popUp: "Hello, I am pop up 2"
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: "Hello, I am pop up 3"
+    }
+  ]
+
+  // custom Icon
+  const customIcon = new Icon({
+    iconUrl: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1hcC1waW4taWNvbiBsdWNpZGUtbWFwLXBpbiI+PHBhdGggZD0iTTIwIDEwYzAgNC45OTMtNS41MzkgMTAuMTkzLTcuMzk5IDExLjc5OWExIDEgMCAwIDEtMS4yMDIgMEM5LjUzOSAyMC4xOTMgNCAxNC45OTMgNCAxMGE4IDggMCAwIDEgMTYgMCIvPjxjaXJjbGUgY3g9IjEyIiBjeT0iMTAiIHI9IjMiLz48L3N2Zz4=",
+    iconSize: [20, 20] // size of the icon
+  })
   return (
+    // location of paris --> {[48.8566, 2.3522]}
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
       <TileLayer 
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {markers.map(marker => (
+        <Marker
+          key={marker.geocode}
+          position={marker.geocode}
+          icon={customIcon}
+        >
+          <Popup 
+            
+          >
+
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   )
 }
