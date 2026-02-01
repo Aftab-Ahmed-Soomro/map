5ebb41f9b88084163f2fb1a34ca3ba45c15ebf4f
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2026-02-01 16:02:43 +0500

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
-import { Icon } from 'leaflet'
-import { MapPin } from 'lucide-react'
+import { Icon, divIcon, point } from 'leaflet'
+import MarkerClusterGroup  from "react-leaflet-cluster"
+import 'react-leaflet-cluster/dist/assets/MarkerCluster.css'
+import 'react-leaflet-cluster/dist/assets/MarkerCluster.Default.css'
 
 const App = () => {
 
@@ -26,6 +28,15 @@ const App = () => {
     iconUrl: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1hcC1waW4taWNvbiBsdWNpZGUtbWFwLXBpbiI+PHBhdGggZD0iTTIwIDEwYzAgNC45OTMtNS41MzkgMTAuMTkzLTcuMzk5IDExLjc5OWExIDEgMCAwIDEtMS4yMDIgMEM5LjUzOSAyMC4xOTMgNCAxNC45OTMgNCAxMGE4IDggMCAwIDEgMTYgMCIvPjxjaXJjbGUgY3g9IjEyIiBjeT0iMTAiIHI9IjMiLz48L3N2Zz4=",
     iconSize: [20, 20] // size of the icon
   })
+
+  // custom cluster Icon
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className: "custom-marker-cluster",
+      iconSize: point(33, 33, true)
+    })
+  }
   return (
     // location of paris --> {[48.8566, 2.3522]}
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
@@ -33,20 +44,24 @@ const App = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
-      {markers.map(marker => (
-        <Marker
-          key={marker.geocode}
-          position={marker.geocode}
-          icon={customIcon}
-        >
-          <Popup 
-            
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction = {createCustomClusterIcon}
+      >
+        {markers.map(marker => (
+          <Marker
+            key={marker.geocode}
+            position={marker.geocode}
+            icon={customIcon}
           >
+            <Popup 
 
-          </Popup>
-        </Marker>
-      ))}
+            >
+              <h2>{marker.popUp}</h2>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>  
     </MapContainer>
   )
 }
