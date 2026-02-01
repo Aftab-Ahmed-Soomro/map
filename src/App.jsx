92a62dcb00b31dbbe8fd5e968cf613e2a0ce10a6
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2026-02-01 17:11:42 +0500

    map

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,15 @@ const App = () => {
   // markers
   const markers = [
     {
-      geocode: [48.86, 2.3522],
+      geocode: [24.86, 67.0011],
       popUp: "Hello, I am pop up 1"
     },
     {
-      geocode: [48.85, 2.3522],
+      geocode: [24.85, 67.0],
       popUp: "Hello, I am pop up 2"
     },
     {
-      geocode: [48.855, 2.34],
+      geocode: [24.855, 67.0011],
       popUp: "Hello, I am pop up 3"
     }
   ]
@@ -26,7 +26,7 @@ const App = () => {
   // custom Icon
   const customIcon = new Icon({
     iconUrl: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1hcC1waW4taWNvbiBsdWNpZGUtbWFwLXBpbiI+PHBhdGggZD0iTTIwIDEwYzAgNC45OTMtNS41MzkgMTAuMTkzLTcuMzk5IDExLjc5OWExIDEgMCAwIDEtMS4yMDIgMEM5LjUzOSAyMC4xOTMgNCAxNC45OTMgNCAxMGE4IDggMCAwIDEgMTYgMCIvPjxjaXJjbGUgY3g9IjEyIiBjeT0iMTAiIHI9IjMiLz48L3N2Zz4=",
-    iconSize: [20, 20] // size of the icon
+    iconSize: [30, 30] // size of the icon
   })
 
   // custom cluster Icon
@@ -39,7 +39,7 @@ const App = () => {
   }
   return (
     // location of paris --> {[48.8566, 2.3522]}
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+    <MapContainer center={[24.8607, 67.0011]} zoom={13}>
       <TileLayer 
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
